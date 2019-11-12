3d98c93fa6ed08131944d331661d39ad11e24da1
Sebastine <Jisa_Sebastine@Dell.com>
2019-11-11 23:39:25 -0600

    added elasticsearch results

--- src/App.js
+++ src/App.js
@@ -1,68 +1,68 @@
 import React from 'react';
-import { ReactiveBase, DataSearch, ResultCard } from '@appbaseio/reactivesearch';
+import { ReactiveBase, DataSearch, ResultCard, ReactiveList } from '@appbaseio/reactivesearch';
 import './App.css';
 class Main extends React.Component {
   render() {
     return (
       <div className="main-container">
         <ReactiveBase
-          app="KnowYourIncident"
-          credentials="RxIAbH9Jc:6d3a5016-5e9d-448f-bd2b-63c80b401484"
-         >
-          <div className="navbar"> 
+          app="swat"
+          type="incident"
+          url="http://localhost:9200/"
+        >
+        <div className="navbar"> 
           <div className="logo">
             Oscar - Know your incident
           </div>
           <DataSearch
             componentId="mainSearch"
             dataField={["incident_id", "description"]}
-            queryFormat="and"
+            defaultSelected="WEPWPP3900WQ"
+            queryFormat="or"
+            highlight="true"
+            highlightField="incident_id"
             placeholder="Search by Incident ID or Keyword..."
             className="datasearch"
             innerClass={{
               "input": "searchbox",
               "list": "suggestionlist"
             }}
-          />
-          </div>
+          /> 
+        </div>
         <div className={"display"}>
           <div className={"leftSidebar"}>
-            <ResultCard
-              componentId="resultsKnowledgeBase"
-              dataField="KnowledgeBase"
-              react={{
-                and: ["mainSearch"]
-              }}
-              onData={(res) => (
-                {
-                  "id": res.confluence_id,
-                  "description": res.description,
-                  "team": res.team_name
-                }
-              )}
-            />
+            <ResultCard/>
             </div>  
           </div>
           
           <div className={"mainBar"}>
-            <ResultCard
-              componentId="resultsIncident"
-              dataField="Incident"
+            <ReactiveList
+              componentId="results"
+              dataField={['description','incident_id','task_id']}
+              size={3}
+              pagination={true}
               react={{
-                and: ["mainSearch"]
+                  and: ['mainSearch'],
               }}
-              onData={(res) => (
-                {
-                  "id": res.incident_id,
-                  "description": res.description
-                }
+              render={({ data }) => (
+                  <ReactiveList.ResultCardsWrapper>
+                      {data.map(item => (
+                          <ResultCard key={item._id}>
+                              <ResultCard.Image
+                                  src=""
+                              />
+                              <ResultCard.Title
+                                  dangerouslySetInnerHTML={{
+                                      __html: item.incident_id == null ? item.task_id: item.incident_id,
+                                  }}
+                              />
+                              <ResultCard.Description>
+                                  {item.description + ' ' + '*'.repeat(item.incident_id)}
+                              </ResultCard.Description>
+                          </ResultCard>
+                      ))}
+                  </ReactiveList.ResultCardsWrapper>
               )}
-              
-              className="result-data"
-              innerClass={{
-                "image": "result-image",
-                "resultStats": "result-stats"
-              }}
             />
           </div>
           
